91af31dbb7507c93e302c12b301627e852164c6e
Andrea Miola <andreamiola95@gmail.com>
2020-03-05 12:03:42 +0100

    Wrote visualization analysis and export figures graphs.

---
+++ src/analysis/visualizing_data.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+from regression import years_classes
+
+# Import each dataFrame of sub_regression_
+
+for indep in ["exret", "job_Retired"]:
+    coefs = []
+    deviations = []
+    labels = []
+    for years in years_classes:
+        sub_par = pd.read_pickle(
+            "sub_regression_"
+            + indep
+            + "_"
+            + str(years[0])
+            + "_"
+            + str(years[1])
+            + "_par.pkl"
+        )
+        sub_std = pd.read_pickle(
+            "sub_regression_"
+            + indep
+            + "_"
+            + str(years[0])
+            + "_"
+            + str(years[1])
+            + "_std.pkl"
+        )
+        coef = sub_par[1]
+        dev = sub_std[1] * 1.96
+        labels.append("From " + str(years[0]) + " to " + str(years[1]))
+        coefs.append(coef)
+        deviations.append(dev)
+    df_plot = pd.DataFrame([coefs, deviations, labels])
+    exp_name = "CI_" + indep
+    plt.figure()
+    x = range(1, len(df_plot.index) + 1)
+    y = df_plot.iloc[0]
+    yerr = df_plot.iloc[1]
+
+    plt.errorbar(x, y, fmt="bo", yerr=yerr, uplims=True, lolims=True)
+    plt.xticks(range(1, len(df_plot.index) + 1), df_plot.iloc[2], size="small")
+    plt.title(
+        "Confidence intervals of " + indep + " per years classes",
+        fontsize=10,
+        weight="bold",
+        wrap="True",
+    )
+    plt.savefig(exp_name)
